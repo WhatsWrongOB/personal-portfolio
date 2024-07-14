cd907417a98341cf761aaec26d58aacdd7e52ab9
WhatsWrongOB <148737556+WhatsWrongOB@users.noreply.github.com>
2024-07-14 15:02:49 +0500

    first commit

---
+++ assets/index.js
@@ -0,0 +1,175 @@
+const skillsData = [
+  {
+    icon: "/images/html.svg",
+    name: "Html",
+    description:
+      "Proficient in writing semantic HTML and creating well-structured web pages",
+    proficiency: "85",
+  },
+  {
+    icon: "/images/css.svg",
+    name: "Css",
+    description:
+      "Skilled in responsive design, using Flexbox and CSS Grid, and creating animations",
+    proficiency: "80",
+  },
+  {
+    icon: "/images/sass.svg",
+    name: "Sass",
+    description:
+      "Experienced in using SASS for writing maintainable and reusable styles",
+    proficiency: "70",
+  },
+  {
+    icon: "/images/tailwind.svg",
+    name: "Tailwind css",
+    description:
+      "Proficient in using Tailwind CSS for rapid UI development, creating utility-first style",
+    proficiency: "70",
+  },
+  {
+    icon: "/images/js.svg",
+    name: "Javascript",
+    description:
+      "Strong understanding of ES6+ features, DOM manipulation, and event handling",
+    proficiency: "70",
+  },
+  {
+    icon: "/images/ts.svg",
+    name: "TypeScript",
+    description:
+      "Strong proficiency in using TypeScript for static typing, enhancing code quality",
+    proficiency: "70",
+  },
+  {
+    icon: "/images/react.svg",
+    name: "ReactJS",
+    description:
+      "Skilled in building dynamic user interfaces, managing state, and using hooks.",
+    proficiency: "70",
+  },
+  {
+    icon: "/images/node.svg",
+    name: "NodeJS",
+    description:
+      "Competent in building server-side applications and handling asynchronous operations.",
+    proficiency: "70",
+  },
+  {
+    icon: "/images/express.svg",
+    name: "ExpressJS",
+    description:
+      "Proficient in setting up routing, middleware, and creating RESTful APIs",
+    proficiency: "70",
+  },
+  {
+    icon: "/images/mongo.svg",
+    name: "MongoDB",
+    description: "Experienced in using MongoDB for managing NoSQL databases,",
+    proficiency: "70",
+  },
+  {
+    icon: "/images/git.svg",
+    name: "Version Control",
+    description:
+      "Knowledgeable in using Git and GitHub for version control and collaboration.",
+    proficiency: "70",
+  },
+];
+
+const portfolioData1 = [
+  {
+    name: "Dashboard",
+    image: "/images/project-1.jpg",
+    link: "https://github.com/WhatsWrongOB",
+  },
+  {
+    name: "Dashboard",
+    image: "/images/project-2.png",
+    link: "https://github.com/WhatsWrongOB",
+  },
+  {
+    name: "Travel Website",
+    image: "/images/project-3.jpg",
+    link: "https://github.com/WhatsWrongOB",
+  },
+];
+
+const portfolioData2 = [
+  {
+    name: "Dashboard",
+    image: "/images/project-4.png",
+    link: "https://github.com/WhatsWrongOB",
+  },
+  {
+    name: "Dashboard",
+    image: "/images/project-5.png",
+    link: "https://github.com/WhatsWrongOB",
+  },
+  {
+    name: "Travel Website",
+    image: "/images/project-6.png",
+    link: "https://github.com/WhatsWrongOB",
+  },
+];
+
+const portfolioData3 = [
+  {
+    name: "Dashboard",
+    image: "/images/project-7.png",
+    link: "https://github.com/WhatsWrongOB",
+  },
+  {
+    name: "Dashboard",
+    image: "/images/project-8.jpg",
+    link: "https://github.com/WhatsWrongOB",
+  },
+  {
+    name: "Travel Website",
+    image: "/images/project-9.png",
+    link: "https://github.com/WhatsWrongOB",
+  },
+];
+
+const testimonailData = [
+  {
+    name: "Imran Khan",
+    message:
+      "Obaid was hired to create a corporate identity. We were very pleased with the work",
+    explainedMessage:
+      "Obaid was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia.",
+    date: "14 June, 2024",
+  },
+  {
+    name: "Mohsin Khan",
+    message:
+      "Obaid was hired to create a corporate identity. We were very pleased with the work",
+    explainedMessage:
+      "Obaid was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia.",
+    date: "14 June, 2024",
+  },
+  {
+    name: "Shahrukh Khan",
+    message:
+      "Obaid was hired to create a corporate identity. We were very pleased with the work",
+    explainedMessage:
+      "Obaid was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia.",
+    date: "14 June, 2024",
+  },
+  {
+    name: "Salman Khan",
+    message:
+      "Obaid was hired to create a corporate identity. We were very pleased with the work",
+    explainedMessage:
+      "Obaid was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia.",
+    date: "14 June, 2024",
+  },
+];
+
+export {
+  skillsData,
+  portfolioData1,
+  portfolioData2,
+  portfolioData3,
+  testimonailData,
+};
